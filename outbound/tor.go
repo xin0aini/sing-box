f5764513a24610c805a7ca0c52fdb681aed8eb9a
yaotthaha <yaotthaha@foxmail.com>
2023-04-16 12:10:35 +0800

    Fix the problem of long delay time for tor startup

--- outbound/tor.go
+++ outbound/tor.go
@@ -2,6 +2,7 @@ package outbound
 
 import (
 	"context"
+	"github.com/sagernet/sing/common/atomic"
 	"net"
 	"os"
 	"path/filepath"
@@ -35,6 +36,8 @@ type Tor struct {
 	events      chan control.Event
 	instance    *tor.Tor
 	socksClient *socks.Client
+
+	bootDone atomic.Bool
 }
 
 func NewTor(ctx context.Context, router adapter.Router, logger log.ContextLogger, tag string, options option.TorOutboundOptions) (*Tor, error) {
@@ -78,6 +81,7 @@ func NewTor(ctx context.Context, router adapter.Router, logger log.ContextLogger
 		proxy:     NewProxyListener(ctx, logger, dialer.New(router, options.DialerOptions)),
 		startConf: &startConf,
 		options:   options.Options,
+		bootDone:  atomic.Bool{},
 	}, nil
 }
 
@@ -142,20 +146,28 @@ func (t *Tor) start() error {
 			}
 		}
 	}
-	err = torInstance.EnableNetwork(t.ctx, true)
-	if err != nil {
-		return err
-	}
-	info, err := torInstance.Control.GetInfo("net/listeners/socks")
-	if err != nil {
-		return err
-	}
-	if len(info) != 1 || info[0].Key != "net/listeners/socks" {
-		return E.New("get socks proxy address")
-	}
-	t.logger.Trace("obtained tor socks5 address ", info[0].Val)
-	// TODO: set password for tor socks5 server if supported
-	t.socksClient = socks.NewClient(N.SystemDialer, M.ParseSocksaddr(info[0].Val), socks.Version5, "", "")
+
+	go func() {
+		err = torInstance.EnableNetwork(t.ctx, true)
+		if err != nil {
+			t.logger.Error("enable tor network fail: ", err.Error())
+			return
+		}
+		info, err := torInstance.Control.GetInfo("net/listeners/socks")
+		if err != nil {
+			t.logger.Error("get tor socks proxy address fail: ", err.Error())
+			return
+		}
+		if len(info) != 1 || info[0].Key != "net/listeners/socks" {
+			t.logger.Error("get socks proxy address")
+			return
+		}
+		t.logger.Trace("obtained tor socks5 address ", info[0].Val)
+		// TODO: set password for tor socks5 server if supported
+		t.socksClient = socks.NewClient(N.SystemDialer, M.ParseSocksaddr(info[0].Val), socks.Version5, "", "")
+		t.bootDone.Store(true)
+	}()
+
 	return nil
 }
 
@@ -196,6 +208,9 @@ func (t *Tor) Close() error {
 
 func (t *Tor) DialContext(ctx context.Context, network string, destination M.Socksaddr) (net.Conn, error) {
 	t.logger.InfoContext(ctx, "outbound connection to ", destination)
+	if !t.bootDone.Load() {
+		return nil, os.ErrNotExist
+	}
 	return t.socksClient.DialContext(ctx, network, destination)
 }
 
