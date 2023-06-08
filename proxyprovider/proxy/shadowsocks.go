69d792c71935602cf35ddcc0d1af00de906439b5
yaotthaha <yaotthaha@foxmail.com>
2023-06-08 23:21:36 +0800

    proxyprovider: shadowsocks plugin obfs-local support

--- proxyprovider/proxy/shadowsocks.go
+++ proxyprovider/proxy/shadowsocks.go
@@ -3,14 +3,19 @@
 package proxy
 
 import (
+	"bytes"
 	"net"
+	"sort"
 	"strconv"
+	"strings"
 
 	C "github.com/sagernet/sing-box/constant"
 	"github.com/sagernet/sing-box/option"
 	dns "github.com/sagernet/sing-dns"
 	E "github.com/sagernet/sing/common/exceptions"
 	N "github.com/sagernet/sing/common/network"
+
+	"github.com/Dreamacro/clash/common/structure"
 )
 
 type proxyClashShadowsocks struct {
@@ -19,8 +24,10 @@ type proxyClashShadowsocks struct {
 	Cipher   string `yaml:"cipher,omitempty"`
 	Password string `yaml:"password,omitempty"`
 	//
-	UDP        *bool `yaml:"udp,omitempty"`
-	UDPOverTCP bool  `yaml:"udp-over-tcp,omitempty"`
+	Plugin     string         `yaml:"plugin,omitempty"`
+	PluginOpts map[string]any `yaml:"plugin-opts,omitempty"`
+	UDP        *bool          `yaml:"udp,omitempty"`
+	UDPOverTCP bool           `yaml:"udp-over-tcp,omitempty"`
 }
 
 type ProxyShadowsocks struct {
@@ -80,6 +87,32 @@ func (p *ProxyShadowsocks) GenerateOptions() (*option.Outbound, error) {
 		},
 	}
 
+	// plugin
+	switch p.clashOptions.Plugin {
+	case "":
+	case "obfs":
+		opts := simpleObfsOption{}
+		decoder := structure.NewDecoder(structure.Option{TagName: "obfs", WeaklyTypedInput: true})
+		err := decoder.Decode(p.clashOptions.PluginOpts, &opts)
+		if err != nil {
+			return nil, E.Cause(err, "decode obfs plugin options")
+		}
+		args := make(map[string][]string)
+		if opts.Host != "" {
+			args["obfs-host"] = []string{opts.Host}
+		}
+		if opts.Mode != "" {
+			args["obfs"] = []string{opts.Mode}
+		}
+		pluginOptsStr := encodeSmethodArgs(args)
+		opt.ShadowsocksOptions.Plugin = "obfs-local"
+		opt.ShadowsocksOptions.PluginOptions = pluginOptsStr
+	case "v2ray-plugin":
+		return nil, E.New("shadowsocks plugin: ", p.clashOptions.Plugin, " is not supported in sing-box (parse from clash)")
+	default:
+		return nil, E.New("shadowsocks plugin: ", p.clashOptions.Plugin, " is not supported in sing-box")
+	}
+
 	if p.clashOptions.UDP != nil && !*p.clashOptions.UDP {
 		opt.ShadowsocksOptions.Network = N.NetworkTCP
 	}
@@ -107,8 +140,6 @@ func (p *ProxyShadowsocks) GenerateOptions() (*option.Outbound, error) {
 	return opt, nil
 }
 
-//
-
 func (p *ProxyShadowsocks) checkMethod() bool {
 	switch p.clashOptions.Cipher {
 	case "aes-128-gcm":
@@ -132,4 +163,45 @@ func (p *ProxyShadowsocks) checkMethod() bool {
 		return false
 	}
 	return true
+}
+
+type simpleObfsOption struct {
+	Mode string `obfs:"mode,omitempty"`
+	Host string `obfs:"host,omitempty"`
+}
+
+func backslashEscape(s string, set []byte) string {
+	var buf bytes.Buffer
+	for _, b := range []byte(s) {
+		if b == '\\' || bytes.IndexByte(set, b) != -1 {
+			buf.WriteByte('\\')
+		}
+		buf.WriteByte(b)
+	}
+	return buf.String()
+}
+
+func encodeSmethodArgs(args map[string][]string) string {
+	if args == nil {
+		return ""
+	}
+
+	keys := make([]string, 0, len(args))
+	for key := range args {
+		keys = append(keys, key)
+	}
+	sort.Strings(keys)
+
+	escape := func(s string) string {
+		return backslashEscape(s, []byte{'=', ','})
+	}
+
+	var pairs []string
+	for _, key := range keys {
+		for _, value := range args[key] {
+			pairs = append(pairs, escape(key)+"="+escape(value))
+		}
+	}
+
+	return strings.Join(pairs, ";")
 }
